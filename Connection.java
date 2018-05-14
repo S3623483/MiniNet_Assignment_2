a876979b52681a5cea3f9a2bd49acdf475f5f933
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-14 23:58:04 +1000

    Update Connection.java
    I have completely changed the Connection Class for Assignment 2.  The Constructor has been changed to have the following signature: (Person person1, Person person2, String type).  Also, get methods have been added to return each of the objects in the amended method signature.

--- Connection.java
+++ Connection.java
@@ -1,22 +1,42 @@
 
 public class Connection {
 	
-	private Person person;
+	private Person person1;
+	private Person person2;
+	private String type;
 	
 	/**
 	 * This method is the class constructor.
 	 * @param person The person being connected with.
 	 */
-	public Connection(Person person) {
-		this.person = person;
+	public Connection(Person person1, Person person2, String type) {
+		this.person1 = person1;
+		this.person2 = person2;
+		this.type = type;
 	}
 	
 	/**
-	 * This method returns the person involved with a particular connection.
+	 * This method returns the person1 of the connection.
 	 * @return Person The person involved with the connection.
 	 */
-	public Person getPerson() {
-		return person;
+	public Person getPerson1() {
+		return person1;
+	}
+	
+	/**
+	 * This method returns the person2 of the connection.
+	 * @return Person The person involved with the connection.
+	 */
+	public Person getPerson2() {
+		return person2;
+	}
+	
+	/**
+	 * This method returns the type of the connection.
+	 * @return String The type of connection.
+	 */
+	public String getType() {
+		return type;
 	}
 	
 }
