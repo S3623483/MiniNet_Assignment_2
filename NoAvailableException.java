6a705e42a7408f60c4bdb54b95edbcf2951b54a1
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-17 00:16:57 +1000

    Create NoAvailableException.java
    Exception to be thrown when the user tries to create a couple connection and at least one member is already connected with another adult as a couple.

---
+++ NoAvailableException.java
@@ -0,0 +1,9 @@
+
+public class NoAvailableException extends Exception {
+	
+	public NoAvailableException(String errMsg) {
+		super(errMsg);
+		System.out.println("One or both of these members are connected with another adult as a couple");
+	}
+	
+}
