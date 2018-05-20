cf2f12d2054b9f7df915036e05678306dfa38d88
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 22:22:09 +1000

    Update NoSuchAgeException.java
    Added comments.

--- NoSuchAgeException.java
+++ NoSuchAgeException.java
@@ -1,10 +1,20 @@
 
+/**
+ * This Exception is thrown when the user tries to add a new member and
+ * inputs an age outside of the allowable bounds of 0 to 150 (inclusive).
+ * @author Benjamin R Donnelly [S3623483]
+ * @version 1.0
+ * @since 2018-05-15
+ */
 public class NoSuchAgeException extends Exception {
 	
-	public NoSuchAgeException (String errMsg, int age) {
+	/**
+	 * Constructor method for NoSuchAgeException.
+	 * @param errMsg Error message passed to the constructor.
+	 */
+	public NoSuchAgeException (String errMsg) {
 		super(errMsg);
-		System.out.println("You have entered age as " + age);
-		System.out.println("Age can not be negative or greater than 150");
+	
 	}
 	
 }
