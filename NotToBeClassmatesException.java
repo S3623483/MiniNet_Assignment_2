db6cc3a900f9f767ab712768d90024f1e8e201ed
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 22:30:09 +1000

    Update NotToBeClassmatesException.java
    Added comments.

--- NotToBeClassmatesException.java
+++ NotToBeClassmatesException.java
@@ -1,9 +1,19 @@
 
+/**
+ * This Exception is thrown when the user tries to include a Baby
+ * in a Classmate connection.
+ * @author Benjamin R Donnelly [S3623483]
+ * @version 1.0
+ * @since 2018-05-16
+ */
 public class NotToBeClassmatesException extends Exception {
 	
+	/**
+	 * Constructor method for NoSuchAgeException.
+	 * @param errMsg Error message passed to the constructor.
+	 */
 	public NotToBeClassmatesException(String errMsg) {
 		super(errMsg);
-		System.out.println("Invalid Classmate Connection.");
 	}
 	
 }
