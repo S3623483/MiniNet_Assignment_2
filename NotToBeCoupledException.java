c74b2dcf238b0fa7788554926080884ec725d894
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 22:38:00 +1000

    Update NotToBeCoupledException.java
    Added comments.

--- NotToBeCoupledException.java
+++ NotToBeCoupledException.java
@@ -1,9 +1,19 @@
 
+/**
+ * This Exception is thrown when the user tries to include either
+ * a Child and/or a Baby in a Couple connection.
+ * @author Benjamin R Donnelly [S3623483]
+ * @version 1.0
+ * @since 2018-05-16
+ */
 public class NotToBeCoupledException extends Exception {
 	
+	/**
+	 * Constructor method for NotToBeCoupledException.
+	 * @param errMsg Error message passed to the constructor.
+	 */
 	public NotToBeCoupledException(String errMsg) {
 		super(errMsg);
-		System.out.println("Only adult members can be included in couple connections.");
 	}
 	
 }
