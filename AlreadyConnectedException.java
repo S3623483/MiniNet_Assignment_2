c230c8529992cee80efa69e297cfc526422230db
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 21:40:39 +1000

    Update AlreadyConnectedException.java
    Added comments.

--- AlreadyConnectedException.java
+++ AlreadyConnectedException.java
@@ -1,9 +1,19 @@
 
+/**
+ * This Exception is thrown when the user tries to duplicate a connection
+ * in MiniNet. For example, this Exception will be thrown when the user tries
+ * to connection two people as friends who are already friends on MiniNet.
+ * @author Benjamin R Donnelly [S3623483]
+ * @version 1.0
+ */
 public class AlreadyConnectedException extends Exception {
 	
+	/**
+	 * Constructor method for AlreadyConnectedException.
+	 * @param errMsg Error message passed to the constructor.
+	 */
 	public AlreadyConnectedException (String errMsg) {
 		super(errMsg);
-		System.out.println("These people are already connected.");
 	}
 	
 }
