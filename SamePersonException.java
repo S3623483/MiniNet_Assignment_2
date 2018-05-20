e9d2fbb5c2a766f5f21bb67a1b143d857678b052
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 23:00:26 +1000

    Update SamePersonException.java
    Added comments.

--- SamePersonException.java
+++ SamePersonException.java
@@ -1,9 +1,21 @@
 
+/**
+ * This Exception is thrown when the user tries to involve the
+ * same member within a connection transaction.  For example, this
+ * Exception will be thrown if the user tries to connect a member
+ * with themself.
+ * @author Benjamin R Donnelly [S3623483]
+ * @version 1.0
+ * @since 2018-05-16
+ */
 public class SamePersonException extends Exception {
 	
+	/**
+	 * Constructor method for SamePersonException.
+	 * @param errMsg Error message passed to the constructor
+	 */
 	public SamePersonException (String errMsg) {
 		super(errMsg);
-		System.out.println("You are trying to connect the same person");
 	}
 	
 }
