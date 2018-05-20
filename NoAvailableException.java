c0d2093ed34c6d8f4b882483a260e50e55b13f00
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 22:13:20 +1000

    Update NoAvailableException.java
    Added comments.

--- NoAvailableException.java
+++ NoAvailableException.java
@@ -1,9 +1,20 @@
 
+/**
+ * This Exception is thrown when the user tries to connect to adults
+ * as a couple and at least one of them is already connected with another
+ * adult as a couple.
+ * @author Benjamin R Donnelly
+ * @version 1.0
+ * @since 2018-05-16
+ */
 public class NoAvailableException extends Exception {
 	
+	/**
+	 * Constructor method for NoAvailableException.
+	 * @param errMsg Error message passed to the constructor.
+	 */
 	public NoAvailableException(String errMsg) {
 		super(errMsg);
-		System.out.println("One or both of these members are connected with another adult as a couple");
 	}
 	
 }
