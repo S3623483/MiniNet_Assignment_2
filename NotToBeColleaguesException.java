9ffc98a6ed720e5fed5111fd4821a18a7e9b3385
Benjamin Donnelly <S3623483@student.rmit.edu.au>
2018-05-20 22:33:39 +1000

    Update NotToBeColleaguesException.java
    Added comments.

--- NotToBeColleaguesException.java
+++ NotToBeColleaguesException.java
@@ -1,9 +1,19 @@
 
+/**
+ * This Exception is thrown when the user tries to include a Baby
+ * in a Colleague connection.
+ * @author Benjamin R Donnelly [S3623483]
+ * @version 1.0
+ * @since 2018-05-16
+ */
 public class NotToBeColleaguesException extends Exception {
 	
+	/**
+	 * Constructor method for NotToBeColleaguesException.
+	 * @param errMsg Error message passed to the constructor.
+	 */
 	public NotToBeColleaguesException (String errMsg) {
 		super(errMsg);
-		System.out.println("A Baby can not be included in a colleague connection.");
 	}
 	
 }
